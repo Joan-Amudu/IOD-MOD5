4acb9ae1757ca3c580ee7f83e8d5abaee31e260d
Joan <joan.baare@gmail.com>
2023-07-08 16:59:20 +1000

    Add Swagger

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const port = 3000;
 var calculatorRoute = require('./routes/calculatorRoute');
 app.use('/calculator',calculatorRoute)
 
+//Swagger
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+app.use(
+'/api-docs',
+swaggerUi.serve,
+swaggerUi.setup(swaggerDocument)
+);
+
 app.get('/', express.static('public'));
 app.listen(port, ()=>{console.log(`app is runing at http://localhost:${port}`)} )
 
